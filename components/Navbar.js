1806dfab3f9695497717bc0f8d04c32a55ee05c2
Chris <chris.ware.dev@gmail.com>
2022-03-02 14:24:54 -0700

    Navbar Complete

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,14 +4,59 @@ import styles from "../styles/Navbar.module.css";
 import { RiVipCrownFill } from "react-icons/ri";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const openMenu = () => setIsOpen(!isOpen);
+
   return (
     <header className={styles.header}>
       <nav className={styles.navbar}>
         <Link href='/'>
           <span className={styles.navbarLogo}>
-              <RiVipCrownFill />
+            <RiVipCrownFill className={styles.navbarIcon} />
+            <a>Company Name</a>
           </span>
         </Link>
+        <ul
+          onClick={openMenu}
+          className={
+            isOpen === false
+              ? styles.navmenu
+              : styles.navmenu + " " + styles.active
+          }
+        >
+          <li className={styles.navItem}>
+            <Link href='/'>
+              <a onClick={openMenu}>Item 1</a>
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href='/'>
+              <a onClick={openMenu}>Item 2</a>
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href='/'>
+              <a onClick={openMenu}>Item 3</a>
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href='/'>
+              <a onClick={openMenu}>Item 4</a>
+            </Link>
+          </li>
+        </ul>
+        <span
+          className={
+            isOpen === false
+              ? styles.hamburger
+              : styles.hamburger + " " + styles.active
+          }
+          onClick={openMenu}
+        >
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+        </span>
       </nav>
     </header>
   );
