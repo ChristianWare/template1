6be7ccd02bf8f7ee400f6194ab7f341474feea75
Chris <chris.ware.dev@gmail.com>
2022-03-03 14:42:02 -0700

    Landing Page Setup

--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,16 @@
+import Image from "next/image";
 import styles from "../styles/Hero.module.css";
 import Button from "./utils/Button";
+import img1 from "../public/images/b3.jpg";
 
 function Hero() {
   return (
     <header className={styles.heroContainer}>
-      <div className={styles.content1}>
-        <div className={styles.content1A}>
-          <h1>Main headline, put top keyword here.</h1>
-        </div>
-        <div className={styles.content1B}>
+      <div className={styles.content}>
+        <div className={styles.contentLeft}>
+          <h1 className={styles.heading}>
+            Main headline, put top keyword here.
+          </h1>
           <p className={styles.description}>
             Expand on the keyword with more keywords and add a list of related
             services that you do and include the city, state of your service
@@ -19,9 +21,18 @@ function Hero() {
             <Button text='services' href='/' color='primaryOutline' />
           </div>
         </div>
+        <div className={styles.contentRight}>
+          <picture>
+            <Image
+              src={img1}
+              alt=''
+              width={367}
+              height={500}
+              className={styles.heroImage}
+            />
+          </picture>
+        </div>
       </div>
-      <div className={styles.content2}></div>
-      <div className={styles.content3}></div>
     </header>
   );
 }
