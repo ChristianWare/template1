7d5237345f7b137b3aef38e76b534f6d6ca100dd
Chris <chris.ware.dev@gmail.com>
2022-03-09 00:49:27 -0700

    Contact Form

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -1,25 +1,91 @@
 import styles from "../styles/ContactSection.module.css";
-import Button from './utils/Button'
+import Button from "./utils/Button";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsTelephoneForward } from "react-icons/bs";
+import { GrFacebookOption } from "react-icons/gr";
+import { BsInstagram } from "react-icons/bs";
+import { FiYoutube } from "react-icons/fi";
+import { FaLinkedinIn } from "react-icons/fa";
+import { ImPinterest2 } from "react-icons/im";
 
 function ContactSection() {
   return (
     <section className={styles.contactContainer}>
-      <div className={styles.content}>
-        <div className={styles.contentLeft}>
-          <h3 className={styles.heading}>It's never too early to start planning your next hair cut!</h3>
-          <address className={styles.address}>
-          85448 e Thompson Rd <br />
-          Goodyear, AZ 85338
-          </address>
-          <p>602-778-8787</p>
-          <p>email: baber@gmail.com</p>
-          
+      <div className={styles.container}>
+        <div className={styles.contactInfo}>
+          <div className={styles.unnammed}>
+            <h2>Contact Info</h2>
+            <ul className={styles.info}>
+              <li>
+                <span>
+                  <MdOutlineLocationOn className={styles.icon} />
+                </span>
+                <span>
+                  2912 MedowBrooke Road <br />
+                  Los Angeles, CA <br />
+                  90017
+                </span>
+              </li>
+              <li>
+                <span>
+                  <AiOutlineMail className={styles.icon} />
+                </span>
+                <span>lorem@ipsum.com</span>
+              </li>
+              <li>
+                <span>
+                  <BsTelephoneForward className={styles.icon} />
+                </span>
+                <span>602-558-8888</span>
+              </li>
+            </ul>
+          </div>
+          <ul className={styles.sci}>
+            <li>
+              <GrFacebookOption className={styles.icon} />
+            </li>
+            <li>
+              <BsInstagram className={styles.icon} />
+            </li>
+            <li>
+              <FiYoutube className={styles.icon} />
+            </li>
+            <li>
+              <FaLinkedinIn className={styles.icon} />
+            </li>
+            <li>
+              <ImPinterest2 className={styles.icon} />
+            </li>
+          </ul>
         </div>
-        <div className={styles.contentRight}>
-        Form Details will go here
-        <div className={styles.btnContainer}>
-            <Button text='submit' href='/' color='secondary'   />
-        </div>
+        <div className={styles.contactForm}>
+          <h2>Send A Message</h2>
+          <div className={styles.formBox}>
+            <div className={styles.inputBox50}>
+              <input type='text' required />
+              <span>First Name</span>
+            </div>
+            <div className={styles.inputBox50}>
+              <input type='text' required />
+              <span>Last Name</span>
+            </div>
+            <div className={styles.inputBox50}>
+              <input type='email' required />
+              <span>Email Address</span>
+            </div>
+            <div className={styles.inputBox50}>
+              <input type='text' required />
+              <span>Phone #</span>
+            </div>
+            <div className={styles.inputBox100}>
+              <textarea required></textarea>
+              <span>Your Message Here...</span>
+            </div>
+            <div className={styles.btnContainer}>
+              <Button text='Submit' color='secondary' />
+            </div>
+          </div>
         </div>
       </div>
     </section>
